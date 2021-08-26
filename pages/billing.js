a1c929efdf249cdacda3092fec20c37a63b921e7
BegzodProgrammer <hamdamovbegzod0520@gmail.com>
2021-08-26 10:38:59 +0500

    billing

---
+++ pages/billing.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Billing = () => {
+  return (
+    <div className="container-fluid py-4">
+      <div className="row">
+        <div className="col-lg-8">
+          <div className="row">
+            <div className="col-xl-6 mb-xl-0 mb-4">
+              <div className="card bg-transparent shadow-xl">
+                <div className="overflow-hidden position-relative border-radius-xl"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4"></div>
+      </div>
+      <div className="row"></div>
+    </div>
+  );
+};
+
+export default Billing;

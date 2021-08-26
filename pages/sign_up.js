83dd879aa9c2f51aab19d3a8a7d0824a9d46b5ff
Samandar2005 <86775489+Samandar2005@users.noreply.github.com>
2021-08-26 11:16:49 +0500

    main

--- pages/sign_up.js
+++ pages/sign_up.js
@@ -3,7 +3,31 @@ import React from "react";
 const Sing_up = () => {
   return (
     <div>
-      <h1>Sing_up</h1>
+      <nav className="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100">
+        <div className="container">
+          <div>
+            <ul className="navbar-nav navbar-nav-hover mx-auto">
+              <li className="nav-item dropdown dropdown-hover mx-2">
+                <a
+                  role="button"
+                  className="d-flex justifay-content-betwen cursore-pointer"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Pages
+                </a>
+                <img src="" alt="dropdovn" className="arrow ms-1 d-lg-block" />
+              </li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <section>
+        <div className="container"></div>
+      </section>
     </div>
   );
 };

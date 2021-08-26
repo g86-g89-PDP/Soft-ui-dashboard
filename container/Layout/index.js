52068b89457f071bdf121831f996b06450452164
samandar <samandarodilboyev@gmail.com>
2021-08-26 10:03:30 +0500

    added new packages

---
+++ container/Layout/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout = (children) => {
+  return <div>Content: {children}</div>;
+};
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Layout from "../container/Layout";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
 
 export default MyApp;

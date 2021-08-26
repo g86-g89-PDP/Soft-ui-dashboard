cae7579f39a49bb8647608403ec7bb9b69d9caa6
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-08-26 11:53:00 +0500

    changing dasfgdfdfg

--- container/Layout/dashboardW.js
+++ container/Layout/dashboardW.js
@@ -8,6 +8,27 @@ const Dashboard=styled.div`
         border-radius: 10px;
         box-shadow: 0 2px 3px  gray;
         margin: 10px;
+        display: flex;
+        .tm3{
+            font-weight: bold;
+            font-size: 14px;
+            line-height: 19px;
+            /* identical to box height */
+            letter-spacing: -0.387234px;
+            color: #82D616;
+            margin-top: 0 !important;
+        }
+        .tmIconsCard{
+            background: linear-gradient(135deg, #FF0080 0%, #7928CA 100%);
+            box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.12), 0px 2px 4px -1px rgba(0, 0, 0, 0.07);
+            border-radius: 8px;
+            width: 45px;
+            height: 45px;
+            margin: auto !important;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+        }
     }
     .roketCard{
         .rokeImg{
@@ -17,6 +38,27 @@ const Dashboard=styled.div`
         box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.12), 0px 2px 4px -1px rgba(0, 0, 0, 0.07);
         border-radius: 8px;  
         padding: 15px 0;
+    }
+    .roketInf{
+        .tm1{
+            font-family: Open Sans;
+            font-style: normal;
+            font-weight: 600;
+            font-size: 14px;
+            line-height: 19px;
+            letter-spacing: -0.387234px;
+            color: #67748E;
+        }
+        .tm2{
+            font-family: Open Sans;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 20px;
+            line-height: 27px;
+            letter-spacing: -0.553191px;
+            color: #252F40;
+        }
+       
     }
     
 `;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import  Dashboard  from "../container/Layout/dashboardW";
+import { FaCoins } from "react-icons/fa";
+
 
 export default function Home() {
   return (
@@ -6,11 +8,27 @@ export default function Home() {
         <div className="row">
           <div className="col-lg-3 col-md-6 col-sm-12">
             <div className='todayMoney'>
+             <div>
+                <p>
+                  today's Money
+                </p>
+                <p className='fw-bold fs-4'>
+                  $ 53.000 <span className='tm3'>+ $50</span> 
+                </p>
+             </div>
+             <div className='tmIconsCard'>
+               <p className='p-0 m-0'><FaCoins/></p>
+
+             </div>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-6 col-sm-12">
+            <div className='todayMoney'>
               <p>
                 today's Money
               </p>
               <p className='fw-bold fs-4'>
-                $ 53.000
+                $ 53.000 <span className='tm3'>+ $50</span> 
               </p>
             </div>
           </div>
@@ -20,17 +38,7 @@ export default function Home() {
                 today's Money
               </p>
               <p className='fw-bold fs-4'>
-                $ 53.000
-              </p>
-            </div>
-          </div>
-          <div className="col-lg-3 col-md-6 col-sm-12">
-            <div className='todayMoney'>
-              <p>
-                today's Money
-              </p>
-              <p className='fw-bold fs-4'>
-                $ 53.000
+                $ 53.000 <span className='tm3'>+ $50</span> 
               </p>
               </div>
           </div>
@@ -48,13 +56,16 @@ export default function Home() {
           <div className="col-lg-7 col-sm-12 d-flex">
             <div className='roketInf'>
               <p className='tm1'>Built by developers</p>
-              <p>Soft UI Dashboard</p>
+              <p className='tm2'>Soft UI Dashboard </p>
               <p>From colors, cards, typography to complex elements, you will find the full documentation.</p>
               <p><a href="#">Read more</a></p>
             </div>
             <div className='roketCard'>
               <img src="https://demos.creative-tim.com/soft-ui-dashboard/assets/img/illustrations/rocket-white.png" alt="picture" className='rokeImg'/>
             </div>
+          </div>
+          <div className="col-5">
+            
           </div>
          
         </div>

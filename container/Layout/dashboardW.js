ffa7c533d23351c3f6df9c40a92fe2092dca5b87
Erkin303 <maxmudjonoverkin55@gmail.com>
2021-08-26 11:19:26 +0500

    changing dashboard1

---
+++ container/Layout/dashboardW.js
@@ -0,0 +1,24 @@
+import  styled  from "styled-components";
+
+const Dashboard=styled.div`
+    padding: 10px;
+    transition: 0.3s;
+    .todayMoney{
+        padding: 10px;
+        border-radius: 10px;
+        box-shadow: 0 2px 3px  gray;
+        margin: 10px;
+    }
+    .roketCard{
+        .rokeImg{
+        width: 300px;
+        } 
+        background: linear-gradient(135deg, #FF0080 0%, #7928CA 100%);
+        box-shadow: 0px 4px 6px -1px rgba(0, 0, 0, 0.12), 0px 2px 4px -1px rgba(0, 0, 0, 0.07);
+        border-radius: 8px;  
+        padding: 15px 0;
+    }
+    
+`;
+
+export default Dashboard

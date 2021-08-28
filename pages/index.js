ab08761354b977e3b80b2c26cea2dbe413c1d331
Samandar2005 <86775489+Samandar2005@users.noreply.github.com>
2021-08-28 08:43:56 +0500

    sing_up

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,11 @@
-export default function Home() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100">
-        <div className="container">
-          <div>
-            <ul className="navbar-nav navbar-nav-hover mx-auto">
-              <li className="nav-item dropdown dropdown-hover mx-2">
-                <a
-                  role="button"
-                  className="d-flex justifay-content-betwen cursore-pointer"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Pages
-                </a>
-              </li>
-              <li className="nav-item dropdown dropdown-hover mx-2">
-                <a
-                  role="button"
-                  className="d-flex justifay-content-betwen cursore-pointer"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Authentication
-                </a>
-              </li>
-              <li className="nav-item dropdown dropdown-hover mx-2">
-                <a
-                  role="button"
-                  className="d-flex justifay-content-betwen cursore-pointer"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Applicationsdown-arrow
-                </a>
-              </li>
-              <li className="nav-item dropdown dropdown-hover mx-2">
-                <a
-                  role="button"
-                  className="d-flex justifay-content-betwen cursore-pointer"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Ecommercedown-arrow
-                </a>
-              </li>
-              <li className="nav-item dropdown dropdown-hover mx-2">
-                <a
-                  role="button"
-                  className="d-flex justifay-content-betwen cursore-pointer"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Docsdown-arrow
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <section>
-        <div className="container"></div>
-      </section>
-    </div>
-  );
+// (../assets/img/curved-images/curved14.jpg)
+
+import main from "./sign_up";
+
+import styled from "styled-components";
+
+const DashboardWrapper = styled.div``;
+
+export default function Home1() {
+  return <a href="./sign_up">Hello</a>;
 }

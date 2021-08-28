f511f17c35596706397fe5612be8ac3e47245234
abdulazizfits <abdurakhmonovabdulaziz8@gmail.com>
2021-08-28 11:04:43 +0500

    Abdulaziz

---
+++ styles/TableWrapper.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const TableWrapper = styled.div`
+
+.tablerow{
+   
+    
+}
+.tablecel2{
+    padding-left: 40px;
+  
+}
+.status{
+    font-family:Open Sans ;
+    font-size: 12px !important;
+    padding-left: 15px;
+    padding-right: 15px;
+    text-align: center;
+   background-color: #60d131;
+    border-radius: 10px;
+    color: white;
+    font-weight: bold;
+}
+.offline {
+    font-family:Open Sans ;
+    font-size: 12px !important;
+    padding-left: 15px;
+    padding-right: 15px;
+    text-align: center;
+    background-color: silver;
+    border-radius: 10px;
+    color: white;
+    font-weight: bold;
+}
+`;
+export default TableWrapper
